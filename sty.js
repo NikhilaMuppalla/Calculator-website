6d2eb7212196ed76ada66dd84b7595e11616039b
NikhilaMuppalla <100996695+NikhilaMuppalla@users.noreply.github.com>
2023-07-26 06:25:35 -0400

    Update sty.js

--- sty.js
+++ sty.js
@@ -1,5 +1,20 @@
 let string = "";
 let buttons = document.querySelectorAll('.button');
+function fact(num) {     
+    if (num < 0){ 
+       return -1; 
+    } 
+    else if(num == 0){ 
+       return 1; 
+    } 
+    else { 
+       let result = 1; 
+       for(var i = num; i > 1; i--){ 
+          result *= i; 
+       }; 
+       return result; 
+    } 
+ }; 
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
@@ -11,21 +26,50 @@ Array.from(buttons).forEach((button) => {
             string = ""
             document.querySelector('input').value = string;
         }
+        else if(e.target.innerHTML=='!'){
+            string=eval(string);
+            Ans=fact(string);
+            document.querySelector('input').value=Ans;
+        }
 
         else {
             if (e.target.innerHTML == 'BS') {
                 string = string.replace(string.charAt(string.length - 1), "");
                 document.querySelector('input').value = string;
             }
+            
             else {
-                if (e.target.innerHTML == 'sin') {
-                    Ans = Math.sin(parseInt(string));
+                if (e.target.innerHTML == 'sin' || e.target.innerHTML=='cos' || e.target.innerHTML =='tan' || e.target.innerHTML =='cosec' || e.target.innerHTML=='sec' || e.target.innerHTML=='cot') {
+                    string=eval(string);
+                    string=string*Math.PI/180
+                    if(e.target.innerHTML=='sin'){
+                        Ans = Math.sin(string);
+                    }
+                    else if(e.target.innerHTML=='cos'){
+                        Ans=Math.cos(string);
+                    }
+                    else if(e.target.innerHTML=='tan'){
+                        Ans=Math.tan(string);
+                    }
+                    else if(e.target.innerHTML=='cosec'){
+                        Ans=1/Math.sin(string);
+                    }
+                    else if(e.target.innerHTML=='sec'){
+                        Ans=1/Math.cos(string);
+                    }
+                    else if(e.target.innerHTML=='cot'){
+                        Ans=1/Math.tan(string);
+                    }
                     document.querySelector('input').value = Ans;
                     string = Ans;
                 }
+               
                 //console.log(e.target)
-                string = string + e.target.innerHTML;
-                document.querySelector('input').value = string;
+                else{
+                    string = string + e.target.innerHTML;
+                    document.querySelector('input').value = string;
+                }
+                
             }
 
         }
